35c9fe33a6ba880bfdd8cd5ec491a61dbad33c8b
wanesoft <wanesoft@mail.ru>
2019-06-05 16:17:28 +0300

    just makeup

--- source/begin.c
+++ source/begin.c
@@ -35,22 +35,13 @@ void			begin(t_vector **env)
 	char		*tmp;
 	char		**arr_str;
 	int			i;
-
-    static int j = 1000;
-    
+	
 	signal(SIGINT, ft_restart);
 	g_env = env;
 	rl_attempted_completion_function = ft_complete;
 	rl_bind_key('\t', rl_complete);
-	/*if ((str = readline("\033[32mMishinshell:\033[0m ")))
-		add_history(str);*/
-    if (j) {
-        str = ft_strdup("cd ~");
-		ft_printf("%d, ", j);
-        --j;
-    } else {
-        str = ft_strdup("exit");
-    }
+	if ((str = readline("\033[32mMishinshell:\033[0m ")))
+		add_history(str);
 	arr_str = ft_strsplit(str, ';');
 	i = 0;
 	while (arr_str[i])
