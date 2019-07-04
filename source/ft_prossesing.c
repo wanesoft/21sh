9378be117188ff2c5b27117084abfbddf3b6db9f
wanesoft <wanesoft@mail.ru>
2019-07-04 15:35:12 +0300

    1

--- source/ft_prossesing.c
+++ source/ft_prossesing.c
@@ -12,8 +12,6 @@
 
 #include "../include/twenty_one_sh.h"
 
-
-
 void		ft_prossesing(char **str, t_vector **env)
 {
 	char	*prep_pipes;
@@ -23,21 +21,8 @@ void		ft_prossesing(char **str, t_vector **env)
 		ft_error(REDICTFAIL, "");
 		return ;
 	}
-	/* DEMO FOR MISHA ->>>>> DEL WHEN POJMESH' */
-	//*str = ft_strdup("echo \"111\" \"222\" \"333\" \"sdfsdgf\" \"\" \"\" \"\" \"\" \"\" \"\" \"\" \"ZZZ\"");
-	ft_printf("****** DEMO FOR MISHA ->>>>> DEL WHEN POJMESH' *******\n");
-	ft_printf("PRISHLO: %s\n", *str);
 	ft_grab(str, 0, 0);
-	ft_printf("IZMENIL: %s\n", *str);
-	t_stack *t = (ft_get_mygv(NULL))->grab;
-	for (int i = 1; t; t = t->next, ++i)
-		ft_printf("SPIZDIL %s POD NOMEROM %d\n", t->data, i);
 	char *sam_razbirajsja_s_mallocom_blo = ft_ungrab(*str, 0);
-	ft_printf("/// A TEPER' NAZAD --------- %s\n", sam_razbirajsja_s_mallocom_blo);
-	ft_printf("****** DEMO IS END    ->>>>> DEL WHEN POJMESH' *******\n");
-	/* DEMO FOR MISHA ->>>>> DEL WHEN POJMESH' */
-	
-	//ft_grab(*str); <<----- vklyuchaet vora kavychek
 	ft_space_for_redirs(str);
 	ft_replacment(str, env);
 	prep_pipes = 0;
